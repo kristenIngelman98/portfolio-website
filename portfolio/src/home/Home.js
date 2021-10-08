85ae9fc76685831cbfc7e09bdb6b1732e8ac1b4c
Kristen <55934200+kristenIngelman98@users.noreply.github.com>
2021-10-08 10:20:11 -0700

    added typewriter animation

--- portfolio/src/home/Home.js
+++ portfolio/src/home/Home.js
@@ -1,18 +1,15 @@
 import * as React from 'react';
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+// import { App } from '../App.css';
 
 function Home() {
-  return (
-    <div className="App">
-     <h1>home page</h1>
-     <li>
-        <Link to="/about">About</Link>
-    </li>
-    <li>
-        <Link to="/">Home</Link>
-    </li>
-    </div>
-  );
+    return (
+        <div className="home_wrapper">
+            <div className="line-1 anim-typewriter">
+                <p>Hi there, nice to meet you! I am Kristen, a Front-End Developer.</p>
+            </div> 
+        </div>
+    );
 }
 
 export default Home;
